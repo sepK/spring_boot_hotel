ee608cbfbc377dadcc69b33f899611a86c81a619
sepK <1351808875@qq.com>
2019-03-15 00:35:42 +0800

    hotel dev version 0.1.6

--- hotel-web/src/test/java/com/ecjtu/kongtao/HotelWebApplicationTests.java
+++ hotel-web/src/test/java/com/ecjtu/kongtao/HotelWebApplicationTests.java
@@ -3,14 +3,21 @@ package com.ecjtu.kongtao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HotelWebApplicationTests {
 
+	@Resource
+	private RedisTemplate<String, String> redisTemplate;
 	@Test
-	public void contextLoads() {
+	public void testSet() {
+		this.redisTemplate.opsForValue().set("study", "java");
+		System.out.println(this.redisTemplate.opsForValue().get("study"));
 	}
 
 }
